5d6abe2879eb6d5a634f7d95da350a0cf147c45a
kapildevpal <kapildevpalone@gmail.com>
2023-11-22 14:55:24 +0530

    Fix device issue fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   devise_for :users, controllers: {
+#     sessions: 'users/sessions'
+#     # Add other controllers here if you generated them and want to customize
+#   }
+#     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+Rails.application.routes.draw do
+  # Your other routes...
+
+  devise_for :users
+
+  # Additional custom routes or resources can be added here...
+
+  # Define a root route
+  root 'welcome#index'
 end
